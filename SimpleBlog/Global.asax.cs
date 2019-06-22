14bc1ed9f20cdd3bc4fa5442de75654bc95114fe
maciej@dziendziel.com <maciej@dziendziel.com>
2019-06-22 18:00:01 +0200

    Imported styles and scripts. jquery and bootstrap

--- SimpleBlog/Global.asax.cs
+++ SimpleBlog/Global.asax.cs
@@ -7,6 +7,8 @@ using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
 using System.Web.Http;
+using System.Web.Optimization;
+using SimpleBlog.App_Start;
 
 namespace SimpleBlog
 {
@@ -17,7 +19,8 @@ namespace SimpleBlog
             // Code that runs on application startup
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);            
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
