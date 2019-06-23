da8c1c3c7c575c2b41e56bc023e6ab94bf274aa0
maciej@dziendziel.com <maciej@dziendziel.com>
2019-06-23 16:39:22 +0200

    Added FluentMigrator. Db created. Initial migration run.

---
+++ SimpleBlog/Migations/_001_UsersAndRoles.cs
@@ -0,0 +1,37 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace SimpleBlog.Migations
+{
+    [Migration(1)]
+    public class _001_UsersAndRoles : Migration
+    {
+        public override void Down()
+        {
+            Delete.Table("UserRoles");
+            Delete.Table("Users");
+            Delete.Table("Roles");
+        }
+
+        public override void Up()
+        {
+            Create.Table("Users")
+                .WithColumn("UserId").AsInt32().Identity().PrimaryKey()
+                .WithColumn("UserName").AsCustom("VARCHAR(128)")    
+                .WithColumn("Email").AsCustom("VARCHAR(256)")
+                .WithColumn("PasswordHash").AsCustom("VARCHAR(128)");
+
+            Create.Table("Roles")
+                .WithColumn("RoleId").AsInt32().Identity().PrimaryKey()
+                .WithColumn("RoleName").AsCustom("VARCHAR(128)");
+
+            Create.Table("UserRoles")
+                .WithColumn("UserId").AsInt32().ForeignKey("Users", "UserId").OnDelete(Rule.Cascade)
+                .WithColumn("RoleId").AsInt32().ForeignKey("Roles", "RoleId").OnDelete(Rule.Cascade);
+        }
+    }
+}
---
+++ SimpleBlog/Tools/DeployDb-Dev.bat
@@ -0,0 +1,1 @@
+..\..\packages\FluentMigrator.1.1.1.0\tools\Migrate.exe --db=SqlServer --target=..\bin\simpleblog.dll --configPath=..\Web.config -c=MainDb
